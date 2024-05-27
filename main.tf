9628b3f9feb0ff35bd433b06f7a4cf4cf4e0b7fa
Daniel Ortiz Aristizábal <daniel.ortiza.col@upb.edu.co>
2024-05-27 12:50:02 -0500

    todo ok

--- main.tf
+++ main.tf
@@ -2,12 +2,10 @@ provider "aws" {
  region = "us-east-1"  # Cambia a tu región AWS deseada
 }
 
-
 resource "aws_key_pair" "namdeployer_keye" {
  key_name   = "llavesnake"
  public_key = file("~/.ssh/llavesnake.pub")
 }
-
 
 # Creación de la VPC
 resource "aws_vpc" "snake_vpc" {
@@ -16,26 +14,6 @@ resource "aws_vpc" "snake_vpc" {
    Name = "Snake-VPC"
  }
 }
-
-
-# Internet Gateway para la VPC
-resource "aws_internet_gateway" "snake_igw" {
- vpc_id = aws_vpc.snake_vpc.id
-
-
- tags = {
-   Name = "Snake-Internet-Gateway"
- }
-}
-
-
-# Ruta para el tráfico de Internet
-resource "aws_route" "snake_public_internet_route" {
- route_table_id         = aws_vpc.snake_vpc.main_route_table_id
- destination_cidr_block = "0.0.0.0/0"
- gateway_id             = aws_internet_gateway.snake_igw.id
-}
-
 
 # Subnet Pública
 resource "aws_subnet" "snake_public_subnet" {
@@ -47,6 +25,22 @@ resource "aws_subnet" "snake_public_subnet" {
  }
 }
 
+# Internet Gateway para la VPC
+resource "aws_internet_gateway" "snake_igw" {
+ vpc_id = aws_vpc.snake_vpc.id
+
+
+ tags = {
+   Name = "Snake-Internet-Gateway"
+ }
+}
+
+# Ruta para el tráfico de Internet
+resource "aws_route" "snake_public_internet_route" {
+ route_table_id         = aws_vpc.snake_vpc.main_route_table_id
+ destination_cidr_block = "0.0.0.0/0"
+ gateway_id             = aws_internet_gateway.snake_igw.id
+}
 
 # Subnet Privada
 resource "aws_subnet" "snake_private_subnet" {
@@ -58,15 +52,12 @@ resource "aws_subnet" "snake_private_subnet" {
  }
 }
 
-
 # Security Group para la instancia EC2 en la subnet privada
 resource "aws_security_group" "snake_sg" {
  name        = "snake-security-group"
  description = "Security group para la instancia Snake"
 
-
  vpc_id = aws_vpc.snake_vpc.id
-
 
  ingress {
    from_port   = 22
@@ -75,7 +66,6 @@ resource "aws_security_group" "snake_sg" {
    cidr_blocks = ["0.0.0.0/0"]
  }
 
-
  ingress {
    from_port   = 3000
    to_port     = 3000
@@ -83,6 +73,12 @@ resource "aws_security_group" "snake_sg" {
    cidr_blocks = ["0.0.0.0/0"]
  }
 
+ ingress {
+   from_port   = 80
+   to_port     = 80
+   protocol    = "tcp"
+   cidr_blocks = ["0.0.0.0/0"]
+ }
 
  egress {
    from_port   = 0
@@ -92,28 +88,24 @@ resource "aws_security_group" "snake_sg" {
  }
 }
 
-
 # Instancia EC2 en la subnet privada
 resource "aws_instance" "snake_instance" {
- ami           = "ami-0bb84b8ffd87024d8"  # Ubuntu 22.04 AMI, cambia según tu región y AMI
+ ami           = "ami-0e001c9271cf7f3b9"  # Ubuntu 22.04 AMI, cambia según tu región y AMI
  instance_type = "t2.large"
  subnet_id     = aws_subnet.snake_private_subnet.id
  key_name      = "llavesnake"  # Cambia al nombre de tu llave pública en AWS
 
-
  security_groups = [aws_security_group.snake_sg.id]
-
 
  tags = {
    Name = "Snake-Instance"
  }
 
-
  user_data     =<<-EOF
        #!/bin/bash
        sudo apt update
-       sudo apt install ufw
-       sudo ufw allow 3000
+       sudo apt install ufw -y
+       sudo ufw allow 3000/tcp
        sudo ufw enable
        sudo apt install docker-compose -y
        git clone https://github.com/dano796/app.git
@@ -122,18 +114,16 @@ resource "aws_instance" "snake_instance" {
        sudo docker run -d -p 3000:3000 appsnake:v01 npm start
        EOF
 
-
  # Asociar IP pública, ya que la instancia estará en la subred privada
  associate_public_ip_address = true
 }
 
-
 # Crear un balanceador de cargas clásico en la misma VPC
 resource "aws_elb" "snake_elb" {
  name            = "snake-classic-load-balancer"
- subnets         = [aws_subnet.snake_public_subnet.id]  # Ubicar en la subred pública
+ subnets         = [aws_subnet.snake_public_subnet.id, 
+                   aws_subnet.snake_private_subnet.id]  # Ubicar en la subred pública
  security_groups = [aws_security_group.snake_sg.id]  # Usar el mismo grupo de seguridad de la instancia
-
 
  listener {
    instance_port     = 3000
@@ -142,18 +132,15 @@ resource "aws_elb" "snake_elb" {
    lb_protocol       = "HTTP"
  }
 
-
- instances = [aws_instance.snake_instance.id]
-
+ instances = [aws_instance.snake_instance.id]  # Cambio aquí
 
  health_check {
-   target              = "HTTP:3000/index.html"
+   target              = "HTTP:3000/index.html"  # Cambio aquí
    interval            = 30
    timeout             = 5
-   unhealthy_threshold = 2
-   healthy_threshold   = 2
+   unhealthy_threshold = 10
+   healthy_threshold   = 10
  }
-
 
  tags = {
    Name = "Snake-Load-Balancer"
